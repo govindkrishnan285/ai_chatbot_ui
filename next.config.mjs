f62801d2c68e23a89f6d970fa1b0c7d173a4c320
sting-raider <alizaydsab@gmail.com>
2025-10-30 12:50:20 +0530

    Final

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // The assetPrefix tells Next.js where to load static files from in production.
+    assetPrefix: 'http://med-chatbot-static-assets.s3-website.eu-north-1.amazonaws.com', // <-- VERIFY THIS URL
+};
 
-export default nextConfig;
+module.exports = nextConfig;
